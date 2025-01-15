507d6543bc2b627e8a75fb2aa1c90475582cea64
Mikael Antony <mikael11antony@gmail.com>
2025-01-15 08:16:14 -0300

    Create metodoParaArrays.js

---
+++ curso Js (Progamador Web)/aula 12/metodoParaArrays.js
@@ -0,0 +1,14 @@
+const jogadores = [
+    'Biro biro',
+    'Ribamar',
+    'Pelé',
+    'maradona',
+    'Neymar',
+    'CR7',
+    'Vampeta',
+    'Dimitri'
+];
+
+const craques = jogadores.slice(2, 6);
+
+console.log(craques);
