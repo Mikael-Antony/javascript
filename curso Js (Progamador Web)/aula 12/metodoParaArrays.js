e08a3eb190dc42af9b027332fe025f1bda9ceed8
Mikael Antony <mikael11antony@gmail.com>
2025-01-15 13:10:04 -0300

    Update metodoParaArrays.js

--- curso Js (Progamador Web)/aula 12/metodoParaArrays.js
+++ curso Js (Progamador Web)/aula 12/metodoParaArrays.js
@@ -12,3 +12,28 @@ const jogadores = [
 const craques = jogadores.slice(2, 6);
 
 console.log(craques);
+
+
+const txt = 'opa bom dia'
+
+console.log(txt.slice(1, 2));
+
+//
+
+const numeros = [40, 100, 1, 5, 25, 10]
+
+console.log(MaiorNumero(numeros));
+console.log(MenorNumero(numeros));
+console.log(numeros.filter(filtragem));
+
+function MaiorNumero(array) {
+    return Math.max.apply(null, array);
+};
+
+function MenorNumero(array) {
+    return Math.min.apply(null, array);
+};
+
+function filtragem(value, index, array) {
+    return value > 20;
+};
